45bb6805a317da9884e8af421b40c5274c99fb1a
mohibytebrew <mohadesehsayahi1@gmail.com>
2024-06-27 00:59:07 +0430

    note online

---
+++ app/src/main/java/ir/mohadesehsayahi/NoteOnline/MainActivity.kt
@@ -0,0 +1,24 @@
+package ir.mohadesehsayahi.NoteOnline
+
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import android.view.Menu
+import android.view.MenuItem
+import ir.mohadesehsayahi.NoteOnline.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
